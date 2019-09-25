171f611375527c4fdf0d9907dc2afc28eaf351a2
Ruslan <ruslanfilistovich@gmail.com>
2019-09-25 21:48:15 +0300

    Initial Commit

---
+++ lessons8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lessons8
+//
+//  Created by Ruslan on 9/25/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
